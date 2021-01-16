ce0b835aa558e2add7cac32161e475bb902f0769
tommygranstrom <tommy.granstrom@hotmail.se>
2021-01-16 23:47:49 +0100

    Updated so it searches both ways, seems like there is bug where it displays a few duplicates. Added input validation and auto suggestion for correction function.

--- Trouting/routingEngineJs.js
+++ Trouting/routingEngineJs.js
@@ -1,3 +1,9 @@
+var validTime = true;
+var destValid = true;
+var startNodeIdx = 7;
+var endNodeIdx = 9;
+
+
 class PathElement{
     constructor(node,aT,dT,trId,rId){
         this.node = node;
@@ -5,6 +11,92 @@ class PathElement{
         this.depTime = dT;
         this.tripId = trId;
         this.routeId = rId;
+    }
+}
+function appendToSpecs(val){
+    document.getElementById("specsoutPutField").value = val;
+}
+
+function autoFill(tmpIn){
+    if(2<tmpIn.length){
+        var i = 0;
+        while (i<autoList.length){
+            if (tmpIn === autoList[i].slice(0,(tmpIn.length))){
+                appendToSpecs("did you mean: "+autoList[i]);
+            }
+            i = i +1;
+        }
+    }
+}
+
+function getIdx(tmpIn){
+    var i = 0;
+    while(i<autoList.length){
+        if(tmpIn === autoList[i]){
+            return i;
+        }
+        i = i+1;
+    }
+    return -1;
+}
+
+function checkDestinations(){
+    var stNode = document.getElementById("start").value;
+    var enNode = document.getElementById("end").value
+    startNodeIdx = getIdx(stNode); 
+    if(0<=startNodeIdx){
+        // OK
+        appendToSpecs("Travel from input valid");
+        endNodeIdx = getIdx(enNode);
+        if(0<=endNodeIdx){
+            destValid = true;
+            appendToSpecs("Travel to input valid");
+            // OK
+        }else{
+            autoFill(enNode);
+            destValid = false;
+        }
+    }else{
+        autoFill(stNode);
+        destValid = false;
+    }
+}
+
+function checkTravelSpecs(){
+    var startGoTime = document.getElementById("goTime").value;
+    var endGoTime =  document.getElementById("arrTime").value;
+    var startHomeTime = document.getElementById("goBackTime").value;
+    var endHomeTime =  document.getElementById("arrTimeHome").value;
+    if(compTime(startGoTime,endGoTime)){
+        appendToSpecs("Input time is valid");
+        if(compTime(endGoTime,startHomeTime)){
+            if(compTime(startHomeTime,endHomeTime)){
+                appendToSpecs("Input time is valid");
+                validTime = true;
+            }else{
+                appendToSpecs("Cannot arrive before departure");
+                validTime = false;
+            }
+        }else{
+            appendToSpecs("Cannot go home before arrived");
+            validTime = false;
+        }
+    }else{
+        appendToSpecs("Cannot arrive before departure");
+        validTime = false;
+    }
+}
+
+
+function compTime(t1,t2){
+    var time1 = t1.split(":");
+    var time2 = t2.split(":");
+    if (parseInt(time1[0])<parseInt(time2[0])){
+        return true;
+    }else if(parseInt(time1[0])==parseInt(time2[0]) && parseInt(time1[1])<parseInt(time2[1])){
+        return true;
+    }else{
+        return false;
     }
 }
 
@@ -98,20 +190,13 @@ class Graph{
         routes.push(routeId);
         
         if(atNode == destination){
-            
+            this.foundPaths = this.foundPaths +1;
+            this.calculatedPaths.push(Object.assign([], path));
             if (this.foundPaths == 200){
                 
                 this.keepSearch = false;
             }
-            var pathString = "---\n"
-            // console.log("---");
-            for(var i = 0;i<path.length;i++){
-                pathString = pathString + this.patReq[path[i].node]['txtId'] + " "+path[i].arrTime+" "+path[i].depTime+"\n";
-            }
-            pathString = pathString + "---\n";
-            // console.log("---");
-            document.getElementById("outPutField").value += pathString;
-            this.foundPaths = this.foundPaths +1;
+            
         }else if(this.countUnique(routes)<this.maxTransfers){
             var tmp = this.getOkChilds(atNode,goTime,routes,tripId,date);
             for(var i = 0;i<tmp.length;i++){
@@ -144,26 +229,68 @@ class Graph{
         var t0 = new Date().getTime();
         this.allPathsUtil(start,destination,goTime,"","",path,visited,date,routes);
         var t1 = new Date().getTime();
+        console.log(start);
+        console.log(" to ");
+        console.log(destination);
         
         console.log(this.foundPaths+" paths found\n");
-        var specs = "";
-        specs= toString(this.foundPaths)+" paths found\n";
         console.log("Calculated in " + (t1 - t0)/1000 + " seconds.")
         //specs = specs +  "Calculated in " + toString((t1 - t0)/1000) + " seconds.\n";
-        document.getElementById("specsoutPutField").value =specs;
     }
 }
 
 
 function startSearching(){
-    g = new Graph();
-    document.getElementById("outPutField").value = "";
-    var start = parseInt(document.getElementById("start").value); //Stockholm
-    var stop = parseInt(document.getElementById("end").value); //Uppsala
-    var goDate = document.getElementById("goDate").value;
-    var startTime = document.getElementById("goTime").value;
-    var endTime =  document.getElementById("arrTime").value;
-    var maxTransfers = document.getElementById("maxTrc").value;
-    var maxnPaths = 200;
-    g.allPaths(start,stop,goDate,startTime,endTime,maxTransfers,maxnPaths)
+    document.getElementById("outPutFieldH").value ="";
+    document.getElementById("outPutField").value ="";
+    var g = new Graph();
+    var g2 = new Graph();
+    if(destValid==true){
+        if(validTime==true){
+            // console.log("Ready to search from ")
+            // console.log(startNodeIdx);
+            // console.log(" to ");
+            // console.log(endNodeIdx);
+            var startGoTime = document.getElementById("goTime").value;
+            var endGoTime =  document.getElementById("arrTime").value;
+            var goDate =  document.getElementById("goDate").value;
+            var maxTransfers = document.getElementById("maxTrc").value;
+            var maxnPaths = 200;
+            g.allPaths(startNodeIdx,endNodeIdx,goDate,startGoTime,endGoTime,maxTransfers,maxnPaths)
+            for(var j = 0;j<g.calculatedPaths.length;j++){
+                var pathString = "---\n"
+                pt = g.calculatedPaths[j];
+            for(var i = 0;i<pt.length;i++){
+                pathString = pathString + g.patReq[pt[i].node]['txtId'] + " "+pt[i].arrTime+" "+pt[i].depTime+"\n";
+            }
+            pathString = pathString + "---\n";
+            // console.log("---");
+            document.getElementById("outPutField").value += pathString;
+            }
+            g.calculatedPaths = [];
+            
+            startGoTime = document.getElementById("goBackTime").value;
+            endGoTime =  document.getElementById("arrTimeHome").value;
+            g2.allPaths(endNodeIdx,startNodeIdx,goDate,startGoTime,endGoTime,maxTransfers,maxnPaths)
+            for(var j = 0;j<g2.calculatedPaths.length;j++){
+                var pathString = "---\n"
+                pt = g2.calculatedPaths[j];
+            for(var i = 0;i<pt.length;i++){
+                pathString = pathString + g2.patReq[pt[i].node]['txtId'] + " "+pt[i].arrTime+" "+pt[i].depTime+"\n";
+            }
+            pathString = pathString + "---\n";
+            // console.log("---");
+            document.getElementById("outPutFieldH").value += pathString;
+            }
+            g2.calculatedPaths = [];
+            appendToSpecs(g.foundPaths+" go there paths found\n" +g2.foundPaths+" go home paths found\n");
+
+        }else{
+            appendToSpecs("Check time inputs");
+        }
+    }else{
+        appendToSpecs("Check start and end stop");
+    }
+    //g.allPaths(start,stop,goDate,startTime,endTime,maxTransfers,maxnPaths)
+    console.log();
 }
